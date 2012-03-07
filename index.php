32cb5ae4bcb25352d8772f30577246a511016943
Sergey Eremin <yeriomin@gmail.com>
2012-03-08 01:12:25 +0400

    autoPublish flag of vk comments widget set to false

--- index.php
+++ index.php
@@ -631,7 +631,7 @@ $(document).ready(function(){
 		echo '<br/>';
 		echo '<script type="text/javascript">VK.init({apiId: 2843150, onlyWidgets: true});</script>';
 		echo '<div id="vk_comments"></div>';
-		echo '<script type="text/javascript">VK.Widgets.Comments("vk_comments", {limit: 10, width: "496", attach: "*"});</script>';
+		echo '<script type="text/javascript">VK.Widgets.Comments("vk_comments", {limit: 10, width: "496", attach: "*", autoPublish: 0});</script>';
 		fclose($log);
 	}
 	function summary_sort($value0,$value1) #Первичная сортировка - по score, вторичная - по kill, третичная - по death
